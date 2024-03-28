0928f5b9a6e168767c1b0dcdcaeff8be78fcf897
Victor-Maverick <victormsonter@gmail.com>
2024-03-28 17:58:01 +0100

    commit

--- gistLover/src/test/java/africa/semicolon/gistLover/controllers/UserControllerTest.java
+++ gistLover/src/test/java/africa/semicolon/gistLover/controllers/UserControllerTest.java
@@ -1,5 +1,30 @@
 package africa.semicolon.gistLover.controllers;
 
+import africa.semicolon.gistLover.data.repository.UserRepository;
+import africa.semicolon.gistLover.dtos.RegisterRequest;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
 public class UserControllerTest {
-    
+    @Autowired
+    UserController userController;
+    @Autowired
+    UserRepository userRepository;
+    @Test
+    public void createUserTest(){
+        RegisterRequest registerRequest = new RegisterRequest();
+        registerRequest.setFirstName("firstname");
+        registerRequest.setLastName("lastname");
+        registerRequest.setUserName("username");
+        registerRequest.setPassword("password");
+        userController.registerWith(registerRequest);
+        assertThat(userRepository.count(), is(equalTo(1L)));
+    }
 }
