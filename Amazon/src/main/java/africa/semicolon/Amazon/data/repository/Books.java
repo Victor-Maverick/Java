c23843bb2229f0170c2b5b0605bb2faa73855e95
Victor-Maverick <victormsonter@gmail.com>
2024-04-02 14:05:48 +0100

    commit

--- Amazon/src/main/java/africa/semicolon/Amazon/data/repository/Books.java
+++ Amazon/src/main/java/africa/semicolon/Amazon/data/repository/Books.java
@@ -4,13 +4,11 @@ import africa.semicolon.Amazon.data.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface Books extends JpaRepository<Book, String> {
 
-    List<Book> findByAuthor(String author);
-
-    Book findByTitle(String title);
+    boolean findByAuthor(String author);
+    List<Book> findByTitleAndAuthor(String title, String author);
 }
--- Amazon/src/main/java/africa/semicolon/Amazon/services/BookServicesImpl.java
+++ Amazon/src/main/java/africa/semicolon/Amazon/services/BookServicesImpl.java
@@ -1,12 +1,16 @@
 package africa.semicolon.Amazon.services;
 
 import africa.semicolon.Amazon.data.model.Book;
+import africa.semicolon.Amazon.data.model.Report;
 import africa.semicolon.Amazon.data.repository.Books;
 import africa.semicolon.Amazon.dtos.requests.AddBookRequest;
+import africa.semicolon.Amazon.dtos.requests.BorrowRequest;
 import africa.semicolon.Amazon.dtos.responses.AddBookResponse;
 import africa.semicolon.Amazon.exceptions.IsbnExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static africa.semicolon.Amazon.utils.Mapper.map;
 
@@ -21,6 +25,13 @@ public class BookServicesImpl implements BookServices{
         map(book, addBookRequest);
         books.save(book);
         return map(book);
+    }
+
+    @Override
+    public Report requestBookWith(BorrowRequest borrowRequest) {
+        List<Book> book = books.findByTitleAndAuthor(borrowRequest.getTitle(), borrowRequest.getAuthor());
+        boolean existsByTitle = books.findByAuthor(borrowRequest.getAuthor());
+        return null;
     }
 
     private boolean isExistingIsbn(AddBookRequest addBookRequest){
