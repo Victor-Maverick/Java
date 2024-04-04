b0915a7b9bf5bdac6f1e05649ae253e896b6ab1a
Victor-Maverick <victormsonter@gmail.com>
2024-04-04 13:37:06 +0100

    commit

--- Amazon/src/test/java/africa/semicolon/Amazon/controllers/LibraryControllerTest.java
+++ Amazon/src/test/java/africa/semicolon/Amazon/controllers/LibraryControllerTest.java
@@ -2,6 +2,7 @@ package africa.semicolon.Amazon.controllers;
 
 import africa.semicolon.Amazon.data.repository.Librarians;
 import africa.semicolon.Amazon.dtos.requests.RegisterRequest;
+import africa.semicolon.Amazon.exceptions.AmazonAppException;
 import africa.semicolon.Amazon.services.LibraryServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,19 @@ public class LibraryControllerTest {
         readerRequest.setPhoneNumber("08148624877");
         libraryController.registerLibrarian(readerRequest);
         assertEquals(1, librarians.count());
+        RegisterRequest readerRequest2 = new RegisterRequest();
+        readerRequest2.setUsername("username");
+        readerRequest2.setPassword("password");
+        readerRequest2.setAddress("semicolon Sabo");
+        readerRequest2.setPhoneNumber("08148624877");
+        try {
+            libraryController.registerLibrarian(readerRequest2);
+        }
+        catch (AmazonAppException e){
+            assertEquals(e.getMessage(), "username exists");
+        }
+        assertEquals(1, librarians.count());
+
     }
 
 }
